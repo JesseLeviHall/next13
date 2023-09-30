ccc6fd76ba108e6ca2792936112c7c9a306a2c66
jesse h <jesselevihall@gmail.com>
2023-09-30 16:05:34 -0600

    light dark theme functional

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -1,31 +1,52 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 import { useTheme } from "@/context/ThemeProvider";
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger,
-} from "@/components/ui/menubar";
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
+import { themes } from "@/constants";
 
 const Theme = () => {
-  const { theme, setTheme } = useTheme();
+  const { mode, setMode } = useTheme();
   return (
-    <Menubar className="">
+    <Menubar className="relative border-none bg-transparent shadow-none">
       <MenubarMenu>
-        <MenubarTrigger>File</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>
-            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>New Window</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Share</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Print</MenubarItem>
+        <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
+          {mode === "light" ? (
+            <Image src="/assets/icons/sun.svg" alt="light mode" width={20} height={20} className="active-theme" />
+          ) : (
+            <Image src="/assets/icons/moon.svg" alt="dark mode" width={20} height={20} className="active-theme" />
+          )}
+        </MenubarTrigger>
+        <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
+          {themes.map((item) => (
+            <MenubarItem
+              className=" flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
+              key={item.value}
+              onClick={() => {
+                setMode(item.value);
+                if (item.value !== "system") {
+                  localStorage.theme = item.value;
+                } else {
+                  localStorage.removeItem("theme");
+                }
+              }}
+            >
+              <Image
+                src={item.icon}
+                alt={item.value}
+                width={16}
+                height={16}
+                className={`${mode === item.value && "active-theme"}`}
+              />
+              <p
+                className={`body-semibold text-light-500 ${
+                  mode === item.value ? "text-primary-500" : "text-dark100_light900"
+                }`}
+              >
+                {item.label}
+              </p>
+            </MenubarItem>
+          ))}
         </MenubarContent>
       </MenubarMenu>
     </Menubar>
