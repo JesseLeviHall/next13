6d34efe109264fd9bf8d5023f8546c064cb25100
jesse h <jesselevihall@gmail.com>
2023-10-20 10:51:04 -0600

    new db connection

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -79,11 +79,12 @@ export const toggleSaveQuestion = async (params: ToggleSaveQuestionParams) => {
   try {
     connectToDatabase();
     const { userId, questionId, path } = params;
+
     const user = await User.findById(userId);
     if (!user) {
       throw new Error("User not found");
     }
-    const isQuestionSaved = user.savedQuestions.includes(questionId);
+    const isQuestionSaved = user.saved.includes(questionId);
     if (isQuestionSaved) {
       await User.findByIdAndUpdate(userId, { $pull: { saved: questionId } }, { new: true });
     } else {
