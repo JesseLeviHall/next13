d6e94107a3eda2daeab098a7138f9af386312ced
jesse h <jesselevihall@gmail.com>
2023-10-26 11:33:21 -0600

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
-
 export default authMiddleware({
+  // "/" will be accessible to all users
   publicRoutes: ["/", "/api/webhook", "question/:id", "/tags", "/tags/:id", "/profile/:id", "/community", "/jobs"],
   ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
