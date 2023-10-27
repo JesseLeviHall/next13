8f49609d2a43906799508b4f3cdc12caf5e58ce7
jesse h <jesselevihall@gmail.com>
2023-10-27 14:53:39 -0600

    possible typo

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   skipJwksCache: true,
   debug: true,
-  publicRoutes: ["/", "/api/webhook", "question/:id", "/tags", "/tags/:id", "/profile/:id", "/community", "/jobs"],
+  publicRoutes: ["/", "/api/webhook", "/question/[id]", "/tags", "/tags/[id]", "/profile/[id]", "/community", "/jobs"],
   ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
